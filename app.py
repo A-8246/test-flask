d1c7fd707b6388b93bd509b78f61269efb553fce
A-8246 <62061147+A-8246@users.noreply.github.com>
2023-03-20 21:35:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,32 @@
-from flask import Flask
+from flask import Flask,request
+
+from langchain.agents import load_tools
+from langchain.agents import initialize_agent
+from langchain.llms import OpenAI
+import os
+
+os.environ['WOLFRAM_ALPHA_APPID'] = 'EWGUTT-U3K3R95PHU'
+os.environ['OPENAI_API_KEY'] = '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'
+
+def out(query):
+	llm = OpenAI(temperature=0)
+	tool_names = ["wolfram-alpha"]
+	tools = load_tools(tool_names)
+	agent = initialize_agent(tools, llm, agent="zero-shot-react-description", verbose=True)
+	res = agent.run(query)
+	return res
+
+
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/qa')
 def hello_world():
-    return 'Hello, World!'
+	args = request.args
+	arg = args.get("query")
+	res = out(arg)
+	return {'bot': res}
+
+
+if __name__ == '__main__':
+    app.run()
